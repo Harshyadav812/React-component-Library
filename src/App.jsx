4c44bf53150af992a1c2c210815ffed315ac5d3e
Harshyadav812 <harshrao812@gmail.com>
2025-09-08 05:18:02 +0000

    Adds Card component into App

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,27 @@ import { useState } from 'react'
 import "./index.css"
 import Badge from './components/Badge/Badge'
 import Banner from './components/Banner/Banner'
+import Card from './components/Card/Card'
 
 function App() {
 
 
   return (
     <main>
-      <Badge color="red">
-        Badge 1
-      </Badge >
       <Badge color={"purple"} shape='pill'>
-        <h1>Badge 2</h1>
+        <h1>Badge 1</h1>
       </Badge>
       <Banner status={"error"} title={"Attention"} />
+
+      <Card>
+        <Card.Icon>
+          <img src="/src/assets/brand.svg" alt="brand icon" />
+        </Card.Icon>
+        <Card.Content>
+          <h3>Easy Deployment</h3>
+          <p>Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</p>
+        </Card.Content>
+      </Card>
     </main>
   )
 }
